d6a8fa195fa15f4f0c8c20130c1dc2f4523627c0
imjamnotjelly <jonahmitre@gmail.com>
2023-04-14 10:38:18 -0400

    finished base functions

--- c_ai/functions.py
+++ c_ai/functions.py
@@ -1,35 +1,41 @@
 # selenium is syntactically goofy
 from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-from c_ai.exeptions import *
+
 
 class Character():
 
     MESSAGE_BOX_ID = 'user-input'
-    TRY_AGAIN_ID = ""
+    LOADING_XPATH = "//button[.//div[@role='status']]"
+    STOP_BUTTON_XPATH = '//button[@aria-label="Stop Message Generation"]'
+    TRY_AGAIN_XPATH = '//button[normalize-space()="Try Again"]'
     
-    def __init__(self, id, headless=False):
+    def __init__(self, id, userdata, headless=False):
         self.id = id
-        self.options = webdriver.FirefoxOptions()
+        self.options = webdriver.ChromeOptions()
+        self.options.add_experimental_option("detach", True)
+        self.options.add_argument(f'user-data-dir={userdata}')
 
         if headless:
             self.options.add_argument('--headless')
             self.options.add_argument('--disable-gpu')
 
     def back(self):
-        self.driver.get(f"https://beta.character.ai/chat?char={self.id})")
+        self.driver.get(rf"https://beta.character.ai/chat?char={self.id}")
 
     def start(self, newchat=False):
-        self.driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=self.options)
+        self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=self.options)
+        self.wait = WebDriverWait(self.driver, 30)
         self.back()
+
         if newchat:
             self.reset()
     
@@ -43,6 +49,7 @@ class Character():
                 continue
             else:
                 return element
+                
     def element_exists(self, by, content):
         try:
             element = self.driver.find_element(by, content)
@@ -50,22 +57,33 @@ class Character():
             return False
         return element
     
-    def prompt(self, message, attempts=5):
-        attempts = int("9"*50) if attempts == 'inf' else attempts
+    def prompt(self, message, attempts=3):
+        attempts = int("9"*10) if attempts == "inf" else attempts
         self.wait_for_element(By.ID, self.MESSAGE_BOX_ID).send_keys(message)
-        for i in range(attempts):
-            if (element:=self.element_exists(By.ID, self.TRY_AGAIN_ID)):
+        for _ in range(attempts):
+            self.wait.until(EC.invisibility_of_element_located((By.XPATH, self.LOADING_XPATH)))
+            self.wait.until(EC.invisibility_of_element_located((By.XPATH, self.STOP_BUTTON_XPATH)))
+            if (element:=self.element_exists(By.ID, self.TRY_AGAIN_XPATH)):
                 element.click()
                 continue
+            return self.driver.find_elements(By.XPATH, '//p[@node="[object Object]"]')[-1]
+            
+            
             
     def retry():
-    
+        pass
+
     def delete():
-    
+        pass
+
     def view():
+        pass
 
     def reset():
+        pass
 
-    def end():
+    def __delete__():
+        pass
 
-
+if __name__ == "__main__":
+    print("https://i.imgur.com/KCSQ0jb.jpeg")
